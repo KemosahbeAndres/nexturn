b09ba6198f0ba3165eb346f204f6f1c2b49d21ae
Andres Cubillos Salazar <andres.cubillos@epchinchorro.cl>
2026-06-11 23:38:19 -0400

    Estilos y plugins base instalados

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from './firebase'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
+app.use(VueFire, {
+  firebaseApp,
+  modules: [VueFireAuth()],
+})
+
+app.mount('#app')
