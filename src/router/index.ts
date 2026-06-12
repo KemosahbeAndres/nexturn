f256abf576a335abb910f9b869c924abab93009c
Andres Cubillos Salazar <andrestj1996@gmail.com>
2026-06-12 00:28:16 -0400

    Layout base y vistas de usuarios y dashboard listas.

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,18 +5,31 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      redirect: '/login'
-    },
-    {
       path: '/login',
       name: 'login',
       component: LoginView
     },
     {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: () => import('../views/DashboardView.vue')
+      path: '/',
+      component: () => import('../layouts/MainLayout.vue'), // Layout Principal
+      children: [
+        {
+          path: '',
+          redirect: '/dashboard' // Redirige por defecto al dashboard
+        },
+        {
+          path: 'dashboard',
+          name: 'dashboard',
+          meta: { title: 'Dashboard' },
+          component: () => import('../views/DashboardView.vue')
+        },
+        {
+          path: 'usuarios',
+          name: 'usuarios',
+          meta: { title: 'Personal y Usuarios' },
+          component: () => import('../views/UsersView.vue')
+        }
+      ]
     }
   ]
 });
